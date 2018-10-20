8bb28179560d18a02f4ab6f166d039391da5dc8a
Bill W <42258241+wwunsch23@users.noreply.github.com>
2018-10-19 23:21:21 -0500

    Updated Javascript for search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,86 @@
 const APPID = "00fded8e";
 const APIKEY = "6c3a70e3044ae190334c8287342f5f18";
-//this is the app.js file
-// 
-// firebase storage 
-//api information
-//response
+
+function titleCase(str) {
+    return str.toLowerCase().split(' ').map(function(word) {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+}
+
+function buildSearchQueryURL() {   
+
+    var queryURL = "https://api.data.charitynavigator.org/v2/Organizations?";
+  
+    var queryParams = {
+        "app_id": APPID, 
+        "app_key": APIKEY    
+    };
+
+    var searchTerm = $("#charityName")
+        .val()
+        .trim();
+    
+    if (searchTerm) {
+        queryParams.search = searchTerm
+    };
+    
+    queryParams.searchType = "NAME_ONLY";
+
+    var state = $("#state")
+        .val()
+        .trim();
+    
+    if (state) {
+        queryParams.state = state
+    };
+
+    var city = $("#city")
+        .val()
+        .trim();
+    
+    if (city) {
+        queryParams.city = city
+    };
+
+    var zip = $("#zipcode")
+        .val()
+        .trim();
+
+    if (zip) {
+        queryParams.zip = zip
+    };
+  
+    //console.log(queryURL + $.param(queryParams));
+    return queryURL + $.param(queryParams);
+}
+  
+
 $(document).ready(function () {
 
     function searchOrganizations() {
-        var settings = {
-            "url": "https://api.data.charitynavigator.org/v2/Organizations?app_id=" + APPID + "&app_key=" + APIKEY,
-            "method": "GET"
-        }
 
-        $.ajax(settings).then(function (response) {
-            console.log("response from api" + response);
+        // Empty the button region
+        $("#infoOne").empty();
+      
+        // Build the query URL for the ajax request to the NYT API
+        var queryURL = buildSearchQueryURL();
+      
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function (response) {
             updatePage(response)
         });
     }
 
     function updatePage(response) {
-        //limit the number of responses?  (default or user selection)
-        // console.log(response);
         var numberOrgs = response.length;
 
         for (var i = 0; i < numberOrgs; i++) {
             // charity name and ein
             var charityName = response[i].charityName;
             var ein = response[i].ein;
-            console.log("Charity Name" + charityName + "--- EIN" + ein)
+            //console.log("Charity Name" + charityName + "--- EIN" + ein)
             createCharityBtns(charityName, ein);
         }
     };
@@ -45,7 +97,9 @@ $(document).ready(function () {
     };
 
     function getOrganization(ein, APPID, APIKEY) {
-        var queryURL = "https://api.data.charitynavigator.org/v2/Organizations/" + ein + "?app_id=" + APPID + "&app_key=" + APIKEY;
+        var orgArea = $("#infoTwo").empty();
+
+        var queryURL = "https://api.data.charitynavigator.org/v2/Organizations/"+ ein + "?app_id=" + APPID + "&app_key=" + APIKEY;
 
         // Performing an AJAX request with the queryURL
         $.ajax({
@@ -54,21 +108,107 @@ $(document).ready(function () {
             })
             // After data comes back from the request
             .then(function (response) {
-                console.log(response);
-// WIP getting vars from api ////
+                //console.log(response);
                 var charName = response.charityName;
                 var tagLine = response.tagLine;
                 var mission = response.mission;
-                // var category = response.category.categoryName;
-                var street = response.streetAddress1;
-                var city = response.city;
-                var state = response.state;
-                var zip = response.zip;
-                var phone = response.phoneNumber;
-                var email = response.generalEmail;
-                var website = response.websiteURL;
 
-                console.log (charName,tagLine,mission,category,street,city,state,zip,phone,email,website);
+                var deductability = response.irsClassification.deductibility;
+                var subsection = response.irsClassification.subsection;
+                var classification = response.irsClassification.classification;
+
+
+                var street = response.mailingAddress.streetAddress1;
+                var city = response.mailingAddress.city;
+                var state = response.mailingAddress.stateOrProvince;
+                var zip = response.mailingAddress.postalCode;
+                var website = response.websiteURL;
+                console.log (charName,tagLine,mission,deductability,subsection,classification,street,city,state,zip,website);
+
+                var newDiv = $("<div>")
+                    .addClass("charityInfo");
+                var charityName = $("<p>")
+                    .attr("id","charName")
+                    .text(charName)
+                    .appendTo(newDiv);
+
+                if (tagLine) {
+                    var tagLineText = $("<p>")
+                        .attr("id","tagLine")
+                        .text(tagLine)
+                        .appendTo(newDiv);
+                };
+                if (mission) {
+                    var missionText = $("<p>")
+                        .attr("id","mission")
+                        .text(mission)
+                        .appendTo(newDiv);
+                };
+
+                if (deductability) {
+                    var deductable = $("<p>")
+                        .attr("id","deductability")
+                        .text("Deduction Status: "+deductability)
+                        .appendTo(newDiv);
+                };
+                if (subsection) {
+                    var subsect = $("<p>")
+                        .attr("id","subsection")
+                        .text("Filing Status: "+subsection)
+                        .appendTo(newDiv);
+                };
+                if (classification) {
+                    var classify = $("<p>")
+                        .attr("id","classification")
+                        .text("Organization Classification: "+classification)
+                        .appendTo(newDiv);
+                };
+
+
+                var addressDiv = $("<div>")
+                    .addClass("fullAddress");
+                if (street) {
+                    var address1 = $("<p>")
+                        .addClass("address1")
+                        .text(titleCase(street))
+                        .appendTo(addressDiv);
+                };
+
+                if (city && state && zip) {
+                    var address2 = $("<p>")
+                        .addClass("address2")
+                        .text(titleCase(city)+", "+state+" "+zip)
+                        .appendTo(addressDiv);
+                } else if (city && state && !zip) {
+                    var address2 = $("<p>")
+                        .addClass("address2")
+                        .text(titleCase(city)+", "+state)
+                        .appendTo(addressDiv);                        
+                } else if (!city && state && zip) {
+                    var address2 = $("<p>")
+                        .addClass("address2")
+                        .text(state+" "+zip)
+                        .appendTo(addressDiv);
+                } else if (!city && state && !zip) {
+                    var address2 = $("<p>")
+                        .addClass("address2")
+                        .text(state)
+                        .appendTo(addressDiv);
+                };
+
+                if (state) {
+                    addressDiv.append(address2);
+                    newDiv.append(addressDiv);
+                };
+
+                if (website) {
+                    var webURL = $("<p>")
+                        .addClass("website")
+                        .text(website)
+                        .appendTo(newDiv);
+                };
+
+                orgArea.append(newDiv);
             })
     };
 
@@ -81,24 +221,23 @@ $(document).ready(function () {
     //////////////////
     $("#search").on("click", function (event) {
         event.preventDefault();
-       searchOrganizations();
-        // console.log("dataResponse" + dataResponse);
-        // updatePage(dataResponse);
+
+        var searchTerm = $("#charityName").val().trim();
+        var state = $("#state").val().trim();
+        var city = $("#city").val().trim();
+        var zip = $("#zipcode").val().trim();
+
+        if (searchTerm || state || city || zip) {
+            searchOrganizations();
+            $("form")[0].reset();
+
+        }
     });
 
 
-    // var ein = "463867129";
     $(document).on("click", ".charity", function () {
         var ein = $(this).attr("id");
-       
         getOrganization(ein, APPID, APIKEY);
     });
-    /////////////////////////////////////Function Calls////////////////////////////////
-    // searchOrganizations();
 
-    //Transfer from api to html
-    //$("#city").
-    //$("#state").
-    //$(#zip).
-    //address
 });
